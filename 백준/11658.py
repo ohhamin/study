6bb903d637021ed9bd7d9ac57b9b6c40603110ab
오하민 <ohm55545@naver.com>
2023-02-08 21:46:55 +0900

    algo

--- 백준/11658.py
+++ 백준/11658.py
@@ -1,18 +1,37 @@
 import sys
 input = sys.stdin.readline
 
+def update(x, y, w):
+    global n
+    while y <= n:
+        tmp_x = x
+        while tmp_x <= n:
+            tree[y][tmp_x] += w
+            tmp_x += tmp_x & -tmp_x
+        y += y & -y
+
+def query(x, y):
+    result = 0
+    while y > 0:
+        tmp_x = x
+        while tmp_x > 0:
+            result += tree[y][tmp_x]
+            tmp_x -= tmp_x & -tmp_x
+        y -= y & -y
+    return result
+        
 n, m = map(int, input().split())
-array = [[0] * (n+1)] + [[0] + list(map(int, input().split())) for _ in range(n)]
-sums = [[0] * (n+1) for _ in range(n+1)]
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        sums[i][j] = sums[i][j-1] + sums[i-1][j] + array[i][j] - sums[i-1][j-1]
+array = [list(map(int, input().split())) for _ in range(n)]
+tree = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(n):
+    for j in range(n):
+        update(i+1, j+1, array[i][j])
+
 for _ in range(m):
     cal = list(map(int, input().split()))
     if cal[0] == 0:
-        tmp_num = cal[3] - array[cal[1]][cal[2]]
-        for i in range(cal[1], n+1):
-            for j in range(cal[2], n+1):
-                sums[i][j] += tmp_num
+        update(cal[1], cal[2], cal[3] - array[cal[1]-1][cal[2]-1])
+        array[cal[1]-1][cal[2]-1] = cal[3]
     else:
-        print(sums[cal[3]][cal[4]] - sums[cal[3]][cal[2]-1] - sums[cal[1]-1][cal[4]] + sums[cal[1]-1][cal[2]-1])
+        print(query(cal[3], cal[4]) - query(cal[3], cal[2]-1) - query(cal[1]-1, cal[4]) + query(cal[1]-1, cal[2]-1))
